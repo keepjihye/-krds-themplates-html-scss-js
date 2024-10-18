f95d5618ddaca2693ad43c2ed0b9f8a7a7097902
keepjihye <yoojihea60@gmail.com>
2024-10-18 17:09:49 +0900

    [keepjihye]24.10.18 ~js까지

---
+++ resources/js/pattern/ui-pattern-script.js
@@ -0,0 +1,24 @@
+// .all_menu클릭하면 #nav_all에 active 추가하기
+$('.all_menu').click(function () {
+    $('#nav-all').addClass("active");
+});
+
+$('#nav-all').click(function () {
+    $('#nav-all').removeClass('active');
+});
+
+$("#gnb").mouseenter(function () {
+    $('header').addClass("active");
+});
+$("#gnb").mouseleave(function () {
+    $('header').removeClass("active");
+});
+
+$(".lang").on({
+    click: function () {
+        $(".lang__lst").slideDown();
+    },
+    mouseleave: function () {
+        $(".lang__lst").slideUp();
+    }
+})
---
+++ resources/js/pattern/ui-swiper.js
@@ -0,0 +1,44 @@
+var swiper = new Swiper(".slide-intro", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    effect: "fade",
+    loop: true,
+    autoplay: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // breakpoints: {
+    //     640: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 20,
+    //     },
+    //     768: {
+    //         slidesPerView: 4,
+    //         spaceBetween: 40,
+    //     },
+    //     1024: {
+    //         slidesPerView: 5,
+    //         spaceBetween: 50,
+    //     },
+    // },
+});
+var swiper = new Swiper(".slide-studio", {
+    // 모바일기준 1개
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
